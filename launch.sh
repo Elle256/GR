928bd0959a5a231d8649df67d7879c1ddf341968
Elle256 <thanhthutran2006@gmail.com>
2026-06-16 00:36:22 +0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -15,10 +15,10 @@ FRAC=0.8
 LOCAL_BS=64
 LR=0.01
 
-ALPHAS=(0.1 0.5)
-DROPOUTS=(0.3)
+ALPHAS=(1)
+DROPOUTS=(0.0 0.5)
 ALGORITHMS=(fedprox)
-SEEDS=(43)
+SEEDS=(42 43)
 
 MU=0.1   # FedProx proximal term
 
